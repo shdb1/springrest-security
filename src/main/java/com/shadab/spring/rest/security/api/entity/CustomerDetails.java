a899d0638a79d742f3da38c42612350a35503bd3
moshadab <moshadab@lm-maa-22017556>
2018-11-25 14:31:40 +0530

    initial commit

---
+++ src/main/java/com/shadab/spring/rest/security/api/entity/CustomerDetails.java
@@ -0,0 +1,37 @@
+package com.shadab.spring.rest.security.api.entity;
+
+import java.io.Serializable;
+
+ 
+public class CustomerDetails implements Serializable {
+	private long id;
+
+	private String name;
+
+	public CustomerDetails() {
+		super();
+	}
+
+	public CustomerDetails(final String name) {
+		super();
+
+		this.name = name;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(final long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(final String name) {
+		this.name = name;
+	}
+
+}
