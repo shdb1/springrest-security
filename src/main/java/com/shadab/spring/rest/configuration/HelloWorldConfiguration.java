128babe6ed42c55db4d8e25d09dbf8ece0de69f5
moshadab <moshadab@lm-maa-22017556>
2018-11-25 16:02:11 +0530

    spring security for rest APIs

---
+++ src/main/java/com/shadab/spring/rest/configuration/HelloWorldConfiguration.java
@@ -0,0 +1,13 @@
+package com.shadab.spring.rest.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.shadab.spring.rest")
+public class HelloWorldConfiguration {
+	
+
+}
---
+++ src/main/java/com/shadab/spring/rest/security/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.shadab.spring.rest.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
